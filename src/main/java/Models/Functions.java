941c4ae7a4e95dab4dc9ee78f03ce1c8686e6ebd
Camillo-commits <58623609+Camillo-commits@users.noreply.github.com>
2021-05-28 15:26:21 +0200

    WIP

--- src/main/java/Models/Functions.java
+++ src/main/java/Models/Functions.java
@@ -1,4 +1,12 @@
 package Models;
 
+import javafx.beans.property.SimpleStringProperty;
+
+import java.util.List;
+
 public class Functions {
+    private SimpleStringProperty function;
+
+    //to make function model for the api and from that create String representation
+    private List<Integer> coefficientList;
 }
